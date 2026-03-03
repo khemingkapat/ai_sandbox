d03e0741ca1340a468205dc2a7a9c668956ddcec
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-03-03 15:41:30 +0700

    remove unused file

--- data/suspend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Slurm passes node names to be suspended as arguments.
-
-for node in "$@"; do
-    nodes_expanded=$(scontrol show hostnames "$node")
-    for expanded_node in $nodes_expanded; do
-        docker stop "$expanded_node"
-    done
-done
