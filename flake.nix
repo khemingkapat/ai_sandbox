f2c1c54f306f1eea248dafd4c2815751a6ae202c
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-03-03 10:36:16 +0700

    redefine nix to only for python

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "HPC Toolkit Local Simulation Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # Stable version
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python3
+            pkgs.uv
+            pkgs.pyright
+            pkgs.black
+          ];
+
+          shellHook = ''
+            export UV_PYTHON_DOWNLOADS=never
+            echo "--- HPC Local Simulation Shell ---"
+            echo "Tools available: docker-compose, uv, python3"
+            echo "Run 'docker-compose up -d' to start your local cluster."
+          '';
+        };
+      }
+    );
+}
