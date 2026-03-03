489ef6e358036ec557abb01dbf858ff92fdee50d
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-03-03 14:21:35 +0700

    fix resume for untimed resume

--- data/resume.sh
+++ data/resume.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# Slurm passes node names as $1 (e.g. worker1,worker2)
-nodes=$(echo $1 | tr ',' ' ')
-docker start $nodes
+# Slurm passes node names (e.g., worker1 worker2) as arguments.
+# This loop ensures each node is started individually.
+
+for node in "$@"; do
+    # Expand brackets if Slurm passes names like worker[1-2]
+    nodes_expanded=$(scontrol show hostnames "$node")
+    for expanded_node in $nodes_expanded; do
+        docker start "$expanded_node"
+    done
+done
--- data/suspend.sh
+++ data/suspend.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-nodes=$(echo $1 | tr ',' ' ')
-docker stop $nodes
+# Slurm passes node names to be suspended as arguments.
+
+for node in "$@"; do
+    nodes_expanded=$(scontrol show hostnames "$node")
+    for expanded_node in $nodes_expanded; do
+        docker stop "$expanded_node"
+    done
+done
