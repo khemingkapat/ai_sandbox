781c3f1fdc26beed49cf8c86f0312af776f242db
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-06-22 22:54:56 +0700

    chore: add go to flake.nix

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -20,6 +20,7 @@
           kind
           kubectl
           kubernetes-helm
+          go
         ];
       in
       {
