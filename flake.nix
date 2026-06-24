81b8da99fac8dc660c79a4077e77d4913c691ebc
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-06-24 22:39:45 +0700

    setup for mult user test

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
